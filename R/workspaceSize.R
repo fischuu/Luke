2e2998db3d54b8fb86d64ec8fc7ed4f02cc663b2
fischuu <github@danielfischer.name>
2018-01-18 10:04:08 +0200

    workspaceSize() function added

---
+++ R/workspaceSize.R
@@ -0,0 +1,9 @@
+workspaceSize <- function(){
+  size <- 0
+  for (x in ls() ){
+    thisSize <- object.size(get(x))
+    size <- size + thisSize
+    message(x, " = ", appendLF = F); print(thisSize, units='auto')
+  }
+  message("total workspace is ",appendLF = F); print(size, units='auto')
+}
