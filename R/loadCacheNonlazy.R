81b3aa938a24890928b842181fe6d05b18c685bf
fischuu <github@danielfischer.name>
2018-04-09 10:11:53 +0300

    Correct environment provided to the function

--- R/loadCacheNonlazy.R
+++ R/loadCacheNonlazy.R
@@ -1,7 +1,7 @@
 loadCacheNonlazy <- function(x){
   rdataFiles <- list.files("../Ruminomics-ReindeerCowMetagenome/Scripts/CowVsReindeerAnalysis_cache/html/", pattern="*.RData")
   
-  for(i in 1:length(rdataDFiles)){
-    load(file.path(x,rdataFiles[i]))
+  for(i in 1:length(rdataFiles)){
+    load(file.path(x,rdataFiles[i]), .GlobalEnv)
   }
 }
