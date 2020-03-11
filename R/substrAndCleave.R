f3816fdf866d040acbbad84a301a499ef7ff6bd8
Daniel Fischer <git@danielfischer.name>
2020-03-11 11:10:42 +0200

    Update substrAndCleave.R

--- R/substrAndCleave.R
+++ R/substrAndCleave.R
@@ -7,9 +7,15 @@ substr.end <- function(x,n){
   substr(x,x.length-n+1, x.length)
 }
 
-cleave.start <- function(x,n){
-  x.length <- nchar(x)
-  substr(x,n+1, x.length)
+cleave.start <- function(x,n=NULL,delim=NULL){
+  if(is.null(n)) stop("Value for n is missing.")
+  if(is.null(delim)){
+    x.length <- nchar(x)
+    out <- substr(x,n+1, x.length)  
+  } else {
+   out <- as.vector(sapply(x, function(x) paste(strsplit(x, delim)[[1]][1:n], collapse = delim)))
+  }
+  out
 }
 
 cleave.end <- function(x,n){
